bc41597cffb115eaebac67e1889a9547d741fad3
btodorov6 <brodorov6@gmail.com>
2025-12-21 14:09:58 +0200

    Added registration function and colouring

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -7,6 +7,32 @@ const char* GREEN = "\033[42m";
 const char* YELLOW = "\033[43m";
 const char* RED = "\033[41m";
 const char* RESET = "\033[0m";
+const char* RED_LETTERS = "\033[91m";
+const char* BOLD = "\033[1m";
+const char* BlINKING_BLACK = "\033[5;40m";
+const char* GREEN_LETTERS = "\033[92m";
+const char* UNDERLINE = "\033[4m";
+int getLengthOfString(char* arr)
+{
+    int counter = 0;
+    while (arr[counter] != '\0')
+    {
+        counter++;
+    }
+    return counter;
+}
+bool doesStringHaveCapitalLetter(char* str)
+{
+    int length = getLengthOfString(str);
+    for (int i = 0;i < length;i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            return true;
+        }
+    }
+    return false;
+}
 char makeCharCapital(char c)
 {
     if (c >= 'a' && c <= 'z')
@@ -52,7 +78,7 @@ void enterUserWord(char* userWord)
     }
     userWord[WORD_LENGTH] = '\0';
 }
-bool areStringsEqual(char* userWord, char* targetWord, char* hints)
+bool areStringsEqual(char* userWord, char* targetWord, char* hints,int i)
 {
     bool isTrue = true;
     for (int i = 0; i < WORD_LENGTH; i++)
@@ -90,17 +116,21 @@ bool areStringsEqual(char* userWord, char* targetWord, char* hints)
             cout << RED << userWord[i] << RESET;
         }
     }
-    cout << endl;
+    if (isTrue == false)
+        cout << endl << BOLD << UNDERLINE << "ROUND" << RESET << BOLD << ' ' << i << RESET << endl;
+    else
+        cout << endl;
     return isTrue;
 }
 void printStartingScreen()
 {
-    cout << " __          ______  _____  _____  _      ______ " << endl
+    cout<< BlINKING_BLACK
+        << " __          ______  _____  _____  _      ______ " << endl
         << " \\ \\        / / __ \\|  __ \\|  __ \\| |    |  ____|" << endl
         << "  \\ \\  /\\  / / |  | | |__) | |  | | |    | |__   " << endl
         << "   \\ \\/  \\/ /| |  | |  _  /| |  | | |    |  __|  " << endl
         << "    \\  /\\  / | |__| | | \\ \\| |__| | |____| |____ " << endl
-        << "     \\/  \\/   \\____/|_|  \\_\\_____/|______|______|" << endl << endl;
+        << "     \\/  \\/   \\____/|_|  \\_\\_____/|______|______|" << RESET << endl << endl;
     cout << "1.Login as an existing user." << endl;
     cout << "2.Register a new user." << endl;
     cout << "3.Exit Program" << endl;
@@ -116,7 +146,42 @@ void exitFunc(bool& finishProgram)
 }
 void registerFunc()
 {
+    ofstream usersFile("users.txt",ios::app );
+    bool isUsernameValid = false;
+    bool isPasswordValid = false;
+    char username[17];
+    char password[17];
+    cout <<BOLD<< ".REGISTERING NEW USER." <<RESET<< endl;
+    cout << RED_LETTERS<< "Username must be between 3 - 16 characters long!"<<endl;
+    cout << "Password must be more than 8 characters and include a capital letter!" <<RESET<< endl;
+    while (!isUsernameValid)
+    {
+        cout << "Enter username: ";
+        cin.getline(username, 17);
 
+        if (getLengthOfString(username) >= 3) {
+            isUsernameValid = true;
+        }
+        else
+        {
+            cout << RED_LETTERS << "Username too short" << RESET << endl;
+        }
+    }
+    while (!isPasswordValid)
+    {
+        cout << "Enter password: ";
+        cin.getline(password, 17);
+        if (getLengthOfString(password) >= 8 && doesStringHaveCapitalLetter(password)) {
+            isPasswordValid = true;
+        }
+        else
+        {
+            cout << RED_LETTERS << "Invalid Password" << RESET << endl;
+        }
+    }
+    usersFile << username << "-" << password << endl;
+    cout << GREEN_LETTERS << "You registered succesfully." << RESET << endl;
+    usersFile.close();
 }
 void loginFunc()
 {
@@ -154,7 +219,7 @@ void chooseRandomWordFromFile(char* word)
 }
 int main()
 {
-    srand(time(0));
+    srand((unsigned int)time(0));
     printStartingScreen();
     char wordToGuess[WORD_LENGTH + 1];
     bool isProgramFinished = false;
@@ -166,13 +231,14 @@ int main()
     } while (startingInput(chooseStartingOperation, isProgramFinished));
     if (!isProgramFinished)
     {
+        cout << BOLD << "GUESS THE WORD" << endl << UNDERLINE << "ROUND" << RESET << BOLD << " 1" << RESET << endl;
         char userWord[WORD_LENGTH + 1];
         char hints[WORD_LENGTH + 1];
         chooseRandomWordFromFile(wordToGuess);
         for (int i = 0;i < 6;i++)
         {
             enterUserWord(userWord);
-            if (areStringsEqual(userWord, wordToGuess,hints))
+            if (areStringsEqual(userWord, wordToGuess,hints,i+2))
             {
                 printWinText();
                 break;
