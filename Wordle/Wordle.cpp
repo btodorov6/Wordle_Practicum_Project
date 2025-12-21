25a41425f852328ecfe4a13df89ce786bd17fb8d
btodorov6 <brodorov6@gmail.com>
2025-12-21 15:58:29 +0200

    Added login,register improvements

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 const int WORD_LENGTH = 5;
 const char* GREEN = "\033[42m";
 const char* YELLOW = "\033[43m";
@@ -10,8 +14,10 @@ const char* RESET = "\033[0m";
 const char* RED_LETTERS = "\033[91m";
 const char* BOLD = "\033[1m";
 const char* BlINKING_BLACK = "\033[5;40m";
+const char* BlINKING_RED_LETTERS = "\033[5;31m";
 const char* GREEN_LETTERS = "\033[92m";
 const char* UNDERLINE = "\033[4m";
+
 int getLengthOfString(char* arr)
 {
     int counter = 0;
@@ -21,10 +27,27 @@ int getLengthOfString(char* arr)
     }
     return counter;
 }
+bool areStringsSame(char* str1, char* str2)
+{
+    int len1 = getLengthOfString(str1);
+    int len2 = getLengthOfString(str2);
+
+    if (len1 != len2) return false;
+
+    for (int i = 0; i < len1; i++)
+    {
+        if (str1[i] != str2[i]) return false;
+    }
+    return true;
+}
+void adminCommands(int& adminInput)
+{
+    
+}
 bool doesStringHaveCapitalLetter(char* str)
 {
     int length = getLengthOfString(str);
-    for (int i = 0;i < length;i++)
+    for (int i = 0; i < length; i++)
     {
         if (str[i] >= 'A' && str[i] <= 'Z')
         {
@@ -43,15 +66,15 @@ char makeCharCapital(char c)
 }
 void printCharArr(char* arr)
 {
-    for (int i = 0;i < WORD_LENGTH;i++)
+    for (int i = 0; i < WORD_LENGTH; i++)
     {
         cout << arr[i];
     }
     cout << endl;
 }
-bool isCharContainedInArr(char c,char* arr)
+bool isCharContainedInArr(char c, char* arr)
 {
-    for (int i = 0;i < WORD_LENGTH;i++)
+    for (int i = 0; i < WORD_LENGTH; i++)
     {
         if (arr[i] == c)
         {
@@ -71,14 +94,26 @@ void printLoseText(char* correctWord)
 }
 void enterUserWord(char* userWord)
 {
-    cin.getline(userWord, WORD_LENGTH + 1);
-    for (int i = 0; i < WORD_LENGTH; i++)
+    bool isWordValid = false;
+    while (!isWordValid)
     {
-        userWord[i] = makeCharCapital(userWord[i]);
+        cin.getline(userWord, 32);
+        if (getLengthOfString(userWord) != 5)
+        {
+            cout << RED_LETTERS << "Word must be exactly 5 letters!" << RESET << endl;
+        }
+        else
+        {
+            isWordValid = true;
+            for (int i = 0; i < WORD_LENGTH; i++)
+            {
+                userWord[i] = makeCharCapital(userWord[i]);
+            }
+            userWord[WORD_LENGTH] = '\0';
+        }
     }
-    userWord[WORD_LENGTH] = '\0';
 }
-bool areStringsEqual(char* userWord, char* targetWord, char* hints,int i)
+bool areStringsEqual(char* userWord, char* targetWord, char* hints, int i)
 {
     bool isTrue = true;
     for (int i = 0; i < WORD_LENGTH; i++)
@@ -124,7 +159,7 @@ bool areStringsEqual(char* userWord, char* targetWord, char* hints,int i)
 }
 void printStartingScreen()
 {
-    cout<< BlINKING_BLACK
+    cout << BlINKING_BLACK
         << " __          ______  _____  _____  _      ______ " << endl
         << " \\ \\        / / __ \\|  __ \\|  __ \\| |    |  ____|" << endl
         << "  \\ \\  /\\  / / |  | | |__) | |  | | |    | |__   " << endl
@@ -137,34 +172,56 @@ void printStartingScreen()
 }
 void invalidInput()
 {
-    cout << "Input is invalid. Choose a number between 1 and 3."<<endl;
+    cout << "Input is invalid. Choose a number between 1 and 3." << endl;
 }
 void exitFunc(bool& finishProgram)
 {
     finishProgram = true;
-    cout << "Exiting Program..."<<endl;
+    cout << BlINKING_RED_LETTERS << "Exiting Program..." << RESET << endl;
 }
+
 void registerFunc()
 {
-    ofstream usersFile("users.txt",ios::app );
+    fstream usersFile("users.txt", ios::in | ios::out | ios::app);
     bool isUsernameValid = false;
     bool isPasswordValid = false;
     char username[17];
     char password[17];
-    cout <<BOLD<< ".REGISTERING NEW USER." <<RESET<< endl;
-    cout << RED_LETTERS<< "Username must be between 3 - 16 characters long!"<<endl;
-    cout << "Password must be more than 8 characters and include a capital letter!" <<RESET<< endl;
+    char userFileInfo[32];
+    cout << BOLD << ".REGISTERING NEW USER." << RESET << endl;
+    cout << RED_LETTERS << "Username must be between 3 - 16 characters long!" << endl;
+    cout << "Password must be more than 8 characters and include a capital letter!" << RESET << endl;
     while (!isUsernameValid)
     {
         cout << "Enter username: ";
         cin.getline(username, 17);
-
-        if (getLengthOfString(username) >= 3) {
-            isUsernameValid = true;
-        }
-        else
-        {
+        if (getLengthOfString(username) < 3) {
             cout << RED_LETTERS << "Username too short" << RESET << endl;
+            continue;
+        }
+        bool exists = false;
+        usersFile.clear();
+        usersFile.seekg(0, ios::beg);
+        while (usersFile >> userFileInfo)
+        {
+            char extractedName[17];
+            int i = 0;
+            while (userFileInfo[i] != '-') {
+                extractedName[i] = userFileInfo[i];
+                i++;
+            }
+            extractedName[i] = '\0';
+
+            if (areStringsSame(username, extractedName)) {
+                exists = true;
+                break;
+            }
+        }
+        if (exists) {
+            cout << RED_LETTERS << "Username already taken! Try another." << RESET << endl;
+        }
+        else {
+            isUsernameValid = true;
         }
     }
     while (!isPasswordValid)
@@ -174,26 +231,96 @@ void registerFunc()
         if (getLengthOfString(password) >= 8 && doesStringHaveCapitalLetter(password)) {
             isPasswordValid = true;
         }
-        else
-        {
+        else {
             cout << RED_LETTERS << "Invalid Password" << RESET << endl;
         }
     }
+    usersFile.clear();
     usersFile << username << "-" << password << endl;
-    cout << GREEN_LETTERS << "You registered succesfully." << RESET << endl;
+    cout << GREEN_LETTERS << "You registered successfully." << RESET << endl;
     usersFile.close();
 }
-void loginFunc()
-{
 
+void loginFunc(bool& isAdmin)
+{
+    ifstream usersFile("users.txt");
+    bool isUsernameFound = false, isPasswordCorrect = false;
+    char username[17];
+    char password[17];
+    char existingUserInformation[32];
+    char existingUsername[17];
+    char existingPassword[17];
+    cout << BOLD << ".LOGGING IN EXISTING USER." << RESET << endl;
+    while (!isUsernameFound)
+    {
+        cout << "Enter username: ";
+        cin.getline(username, 17);
+        while (usersFile >> existingUserInformation)
+        {
+            int i = 0;
+            while (existingUserInformation[i] != '-') {
+                existingUsername[i] = existingUserInformation[i];
+                i++;
+            }
+            existingUsername[i] = '\0';
+
+            int k = 0;
+            i++;
+            while (existingUserInformation[i] != '\0') {
+                existingPassword[k] = existingUserInformation[i];
+                i++;
+                k++;
+            }
+            existingPassword[k] = '\0';
+            if (areStringsSame(username, existingUsername))
+            {
+                isUsernameFound = true;
+                break;
+            }
+        }
+
+        if (!isUsernameFound)
+        {
+            cout << RED_LETTERS << "No such user exists. Try again." << RESET << endl;
+            usersFile.clear();
+            usersFile.seekg(0, ios::beg);
+        }
+    }
+    while (!isPasswordCorrect)
+    {
+        cout << "Enter Password: ";
+        cin.getline(password, 17);
+        if (areStringsSame(existingPassword, password))
+        {
+            isPasswordCorrect = true;
+        }
+        else
+        {
+            cout<< RED_LETTERS << "Incorrect password. Try again" << RESET << endl;
+        }
+    }
+    
+    cout << "Succesfully logged in as ";
+    if (areStringsSame(username, (char*)"administrator"))
+    {
+        cout << "admin" << endl;
+        isAdmin = true;
+    }
+    else
+        cout << username << endl;
+    usersFile.close();
 }
-bool startingInput(const int n, bool& finishProgram)
+
+bool startingInput(const int n, bool& finishProgram, bool& isAdmin)
 {
     switch (n)
     {
     case 1:
-        loginFunc();
-        return false;
+        loginFunc(isAdmin);
+        if (isAdmin)
+            return true;
+        else
+            return false;
     case 2:
         registerFunc();
         return false;
@@ -205,40 +332,44 @@ bool startingInput(const int n, bool& finishProgram)
         return true;
     }
 }
+
 void chooseRandomWordFromFile(char* word)
 {
     ifstream wordFile("words.txt");
     int wordCount = 5;
-    int num = (rand() % wordCount)+1;
-    for (int i = 0;i < num;i++)
+    int num = (rand() % wordCount) + 1;
+    for (int i = 0; i < num; i++)
     {
         wordFile >> word;
     }
     word[WORD_LENGTH] = '\0';
     wordFile.close();
 }
+
 int main()
 {
     srand((unsigned int)time(0));
     printStartingScreen();
     char wordToGuess[WORD_LENGTH + 1];
-    bool isProgramFinished = false;
+    bool isProgramFinished = false, isAdmin = false;
     int chooseStartingOperation = 0;
+
     do {
         cout << "Enter your input: ";
         cin >> chooseStartingOperation;
         cin.ignore();
-    } while (startingInput(chooseStartingOperation, isProgramFinished));
-    if (!isProgramFinished)
+    } while (startingInput(chooseStartingOperation, isProgramFinished, isAdmin));
+
+    if (!isProgramFinished && !isAdmin)
     {
         cout << BOLD << "GUESS THE WORD" << endl << UNDERLINE << "ROUND" << RESET << BOLD << " 1" << RESET << endl;
         char userWord[WORD_LENGTH + 1];
         char hints[WORD_LENGTH + 1];
         chooseRandomWordFromFile(wordToGuess);
-        for (int i = 0;i < 6;i++)
+        for (int i = 0; i < 6; i++)
         {
             enterUserWord(userWord);
-            if (areStringsEqual(userWord, wordToGuess,hints,i+2))
+            if (areStringsEqual(userWord, wordToGuess, hints, i + 2))
             {
                 printWinText();
                 break;
@@ -248,6 +379,19 @@ int main()
                 printLoseText(wordToGuess);
                 break;
             }
+        }
+    }
+
+    if (isAdmin)
+    {
+        int adminInput = 0;
+        cout << BOLD << "You have logged in as an admin." << RESET << endl;
+        while (adminInput != 3)
+        {
+            cout << "1.Add new words" << endl << "2.Look at the leaderboard" << endl << "3.Exit program" << endl;
+            cout << "Enter your input: ";
+            cin >> adminInput;
+            adminCommands(adminInput);
         }
     }
 }
