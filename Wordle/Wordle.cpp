9c10daa134556a68cb83cac13eef8d485ca7411d
btodorov6 <brodorov6@gmail.com>
2025-12-22 17:47:10 +0200

    Implemented the adding to word list and other improvements

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -12,6 +12,8 @@ const char* YELLOW = "\033[43m";
 const char* RED = "\033[41m";
 const char* RESET = "\033[0m";
 const char* RED_LETTERS = "\033[91m";
+const char* DARK_RED_LETTERS= "\033[31m";
+const char* DARK_GREEN_LETTERS = "\033[32m";
 const char* BOLD = "\033[1m";
 const char* BlINKING_BLACK = "\033[5;40m";
 const char* BlINKING_RED_LETTERS = "\033[5;31m";
@@ -20,15 +22,7 @@ const char* BLUE_LETTERS = "\033[94m";
 const char* UNDERLINE = "\033[4m";
 const char* ERASE_LINE = "\033[1A\033[2K";
 const char* CLEAR_SCREEN = "\033[2J\033[H";
-
-void invalidInput()
-{
-    cout << RED_LETTERS << "Input is invalid. Choose a number between 1 and 3 from the options above." << RESET << endl;
-}
-void exitFunc()
-{
-    cout << BlINKING_RED_LETTERS << "Exiting Program..." << RESET << endl;
-}
+const char* BLINKING_GOLD = "\033[5;38;5;220m";
 int getLengthOfString(char* arr)
 {
     int counter = 0;
@@ -37,6 +31,14 @@ int getLengthOfString(char* arr)
         counter++;
     }
     return counter;
+}
+char makeCharCapital(char c)
+{
+    if (c >= 'a' && c <= 'z')
+    {
+        return c - 'a' + 'A';
+    }
+    return c;
 }
 bool areStringsSame(char* str1, char* str2)
 {
@@ -51,11 +53,123 @@ bool areStringsSame(char* str1, char* str2)
     }
     return true;
 }
-void adminCommands(int& adminInput)
+void enterUserWord(char* userWord)
+{
+    bool isWordValid = false;
+    while (!isWordValid)
+    {
+        cin.getline(userWord, 32);
+        if (getLengthOfString(userWord) != 5)
+        {
+            cout << ERASE_LINE;
+            cout << RED_LETTERS << "Word must be exactly 5 letters!" << RESET << endl;
+        }
+        else
+        {
+            isWordValid = true;
+            for (int i = 0; i < WORD_LENGTH; i++)
+            {
+                userWord[i] = makeCharCapital(userWord[i]);
+            }
+            userWord[WORD_LENGTH] = '\0';
+        }
+    }
+}
+void addWords(char* wordsAdded)
+{
+    fstream wordsFile("words.txt", ios::in | ios::out | ios::app);
+    char existingWord[6], wordToAdd[32];
+    bool isWordValid = false;
+    while (!isWordValid)
+    {
+        cout << "Enter word to add: " << endl;
+        enterUserWord(wordToAdd);
+        bool exists = false;
+        wordsFile.clear();
+        wordsFile.seekg(0, ios::beg);
+        while (wordsFile >> existingWord)
+        {
+            if (areStringsSame(existingWord, wordToAdd))
+            {
+                exists = true;
+                break;
+            }
+        }
+        if (exists)
+        {
+            cout << ERASE_LINE;
+            cout << ERASE_LINE;
+            cout << RED_LETTERS << "Such a word already exists" << RESET << endl;
+        }
+        else
+        {
+            wordsFile.clear();
+            wordsFile.seekp(0, ios::end);
+            wordsFile << endl;
+            wordsFile << wordToAdd;
+            isWordValid = true;
+            cout << BLUE_LETTERS << wordToAdd << RESET << GREEN_LETTERS << " successfully added to the list." << RESET << endl;
+        }
+    }
+    int i = 0,j=0;
+    while (wordsAdded[i] != '\0')
+    {
+        ++i;
+    }
+    while (wordToAdd[j] != '\0')
+    {
+        wordsAdded[i] = wordToAdd[j];
+        ++i;
+        ++j;
+    }
+    wordsAdded[i] = ' ';
+    wordsAdded[i + 1] = '\0';
+    wordsFile.close();
+}
+void removeWords(char* wordsRemoved)
+{
+    cout << "removing words..."<<endl;
+}
+void modifyWords(char* wordsAdded,char* wordsRemoved)
+{
+    bool isInputValid = false;
+    int modifyInput;
+    cout << BOLD << "1.Add words" << endl << "2.Remove words" << endl<<"Enter your input: ";
+    cin >> modifyInput;
+    cin.clear();
+    cin.ignore(1000, '\n');
+    while (!isInputValid)
+    {
+        if (modifyInput == 1)
+        {
+            addWords(wordsAdded);
+            isInputValid = true;
+        }
+        else if (modifyInput == 2)
+        {
+            removeWords(wordsRemoved);
+            isInputValid = true;
+        }
+        else
+        {
+            cout << "Input is invalid choose option 1 or 2" << endl;
+        }
+    }
+}
+void invalidInput()
+{
+    cout << RED_LETTERS << "Input is invalid. Choose a number between 1 and 3 from the options above." << RESET << endl;
+}
+void exitFunc()
+{
+    cout << BlINKING_RED_LETTERS << "Exiting Program..." << RESET << endl;
+}
+void adminCommands(int& adminInput,char* wordsAdded,char* wordsRemoved)
 {
     switch (adminInput)
     {
     case 1:
+        modifyWords(wordsAdded,wordsRemoved);
         break;
     case 2:
         break;
@@ -79,19 +193,13 @@ bool doesStringHaveCapitalLetter(char* str)
     }
     return false;
 }
-char makeCharCapital(char c)
-{
-    if (c >= 'a' && c <= 'z')
-    {
-        return c - 'a' + 'A';
-    }
-    return c;
-}
 void printCharArr(char* arr)
 {
-    for (int i = 0; i < WORD_LENGTH; i++)
+    int i = 0;
+    while (arr[i] != '\0')
     {
         cout << arr[i];
+        ++i;
     }
     cout << endl;
 }
@@ -115,28 +223,6 @@ void printLoseText(char* correctWord)
     cout <<RED_LETTERS<< "You ran out of attempts. The word was - ";
     printCharArr(correctWord);
     cout << RESET;
-}
-void enterUserWord(char* userWord)
-{
-    bool isWordValid = false;
-    while (!isWordValid)
-    {
-        cin.getline(userWord, 32);
-        if (getLengthOfString(userWord) != 5)
-        {
-            cout << ERASE_LINE;
-            cout << RED_LETTERS << "Word must be exactly 5 letters!" << RESET << endl;
-        }
-        else
-        {
-            isWordValid = true;
-            for (int i = 0; i < WORD_LENGTH; i++)
-            {
-                userWord[i] = makeCharCapital(userWord[i]);
-            }
-            userWord[WORD_LENGTH] = '\0';
-        }
-    }
 }
 bool areStringsEqual(char* userWord, char* targetWord, char* hints, int i)
 {
@@ -184,7 +270,7 @@ bool areStringsEqual(char* userWord, char* targetWord, char* hints, int i)
 }
 void printASCIIart()
 {
-    cout << BlINKING_BLACK
+    cout << BLINKING_GOLD
         << " __          ______  _____  _____  _      ______ " << endl
         << " \\ \\        / / __ \\|  __ \\|  __ \\| |    |  ____|" << endl
         << "  \\ \\  /\\  / / |  | | |__) | |  | | |    | |__   " << endl
@@ -195,14 +281,32 @@ void printASCIIart()
 void printStartingScreen()
 {
     printASCIIart();
-    cout <<GREEN_LETTERS<< "1.Login as an existing user." << endl<< "2.Register a new user."<<RESET<<endl<< BlINKING_RED_LETTERS<< "3.Exit Program"<<RESET << endl;
+    cout <<DARK_GREEN_LETTERS<< "1.Login as an existing user." << endl<< "2.Register a new user."<<RESET<<endl<< DARK_RED_LETTERS<< "3.Exit Program"<<RESET << endl;
+}
+void printAdminCommands(char* wordsAdded,char* wordsRemoved)
+{
+    cout << CLEAR_SCREEN;
+    printASCIIart();
+    cout << "Successfully logged in as " << BLUE_LETTERS<<"admin"<<RESET<<endl;
+    cout << BLUE_LETTERS << "1.Modify word list" << endl << "2.Look at the leaderboard" << RESET << endl << DARK_RED_LETTERS << "3.Exit program" << RESET << endl;
+    if (wordsAdded[0] != '\0')
+    {
+        cout << "Words added: "<<GREEN_LETTERS;
+        printCharArr(wordsAdded);
+        cout << RESET;
+    }
+    if (wordsRemoved[0] != '\0')
+    {
+        cout << "Words removed: " << RED_LETTERS;
+        printCharArr(wordsRemoved);
+        cout << RESET;
+    }
 }
 void exitFunc(bool& finishProgram)
 {
     finishProgram = true;
     cout << BlINKING_RED_LETTERS << "Exiting Program..." << RESET << endl;
 }
-
 void registerFunc()
 {
     fstream usersFile("users.txt", ios::in | ios::out | ios::app);
@@ -282,10 +386,11 @@ void registerFunc()
     }
     usersFile.clear();
     usersFile << username << "-" << password << endl;
-    cout << GREEN_LETTERS << "You registered successfully." << RESET << endl;
+    cout << CLEAR_SCREEN;
+    printASCIIart();
+    cout << GREEN_LETTERS << "You registered successfully as " << RESET << BLUE_LETTERS << username << RESET << endl;
     usersFile.close();
 }
-
 void loginFunc(bool& isAdmin)
 {
     ifstream usersFile("users.txt");
@@ -345,18 +450,13 @@ void loginFunc(bool& isAdmin)
     }
     cout << CLEAR_SCREEN;
     printASCIIart();
-    cout << "Succesfully logged in as "<<BLUE_LETTERS;
-    if (areStringsSame(username, (char*)"administrator"))
+    if (areStringsSame(username,(char*)"administrator"))
     {
-        cout << "admin";
         isAdmin = true;
     }
-    else
-        cout << username;
-    cout << RESET << endl;
+    cout << "Succesfully logged in as "<<BLUE_LETTERS<<username<<RESET<<endl;
     usersFile.close();
 }
-
 bool startingInput(const int n, bool& finishProgram, bool& isAdmin)
 {
     switch (n)
@@ -375,7 +475,6 @@ bool startingInput(const int n, bool& finishProgram, bool& isAdmin)
         return true;
     }
 }
-
 void chooseRandomWordFromFile(char* word)
 {
     ifstream wordFile("words.txt");
@@ -428,15 +527,16 @@ int main()
 
     if (isAdmin)
     {
+        char wordsAdded[500]="", wordsRemoved[500]="";
         int adminInput = 0;
-        cout <<BLUE_LETTERS<< "1.Add new words" << endl << "2.Look at the leaderboard"<<RESET << endl<<BlINKING_RED_LETTERS << "3.Exit program" <<RESET<< endl;
         while (adminInput != 3)
         {
+            printAdminCommands(wordsAdded,wordsRemoved);
             cout << "Enter your input: ";
             cin >> adminInput;
             cin.clear();
             cin.ignore(1000, '\n');
-            adminCommands(adminInput);
+            adminCommands(adminInput,wordsAdded,wordsRemoved);
         }
     }
 }
